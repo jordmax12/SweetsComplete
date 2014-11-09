aea03740dcdcbdd432c701b6abaaaf1b50678635
Jordan Max <jordan.max@loop.colum.edu>
2014-11-08 19:56:33 -0600

    Member Search Implementation

--- SweetsCompleteApp/Controllers/MembersController.cs
+++ SweetsCompleteApp/Controllers/MembersController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Globalization;
 
 namespace SweetsCompleteApp.Controllers
 {
@@ -15,5 +16,6 @@ namespace SweetsCompleteApp.Controllers
         {
             return View(db.members.ToList());
         }
+    
 	}
 }
