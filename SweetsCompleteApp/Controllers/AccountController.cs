18e9baa437f19c2ea608ab4d8ecc0fb540b95ea8
Jordan Max <jordan.max@loop.colum.edu>
2014-11-10 17:05:24 -0600

    this shoudl work

--- SweetsCompleteApp/Controllers/AccountController.cs
+++ SweetsCompleteApp/Controllers/AccountController.cs
@@ -94,26 +94,7 @@ namespace SweetsCompleteApp.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser() { UserName = model.Email, Email = model.Email };
-                IdentityResult result = await UserManager.CreateAsync(user, model.Password);
-                if (result.Succeeded)
-                {
-                    await SignInAsync(user, isPersistent: false);
-
-                    //InsertToDB(user.Email, user.PasswordHash);
-
-                    // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
-                    // Send an email with this link
-                    // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
-                    // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
-                    // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
-
-                    return RedirectToAction("Index", "Home");
-                }
-                else
-                {
-                    AddErrors(result);
-                }
+               
             }
 
             // If we got this far, something failed, redisplay form
